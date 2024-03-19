0e7de8e31b806519095ac63cdd05a1118bdc4b3d
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-03-19 21:39:26 +0000

    Migrate to git lfs

--- src/sceptr/_model_saves/__init__.py
+++ src/sceptr/_model_saves/__init__.py
@@ -1,28 +1,29 @@
 from importlib import resources
 import json
+from sceptr._lib.sceptr import Sceptr
+from sceptr._lib.config_reader import ConfigReader
 import torch
 
 
-class ModelSave:
-    def __init__(self, config: dict, state_dict: dict) -> None:
-        self.config = config
-        self.state_dict = state_dict
+def load_variant(model_name: str) -> Sceptr:
+    model_save_dir = resources.files(__name__) / model_name
 
-
-def load_model_save(model_name: str) -> ModelSave:
-    model_save = resources.files(__name__) / model_name
-
-    with (model_save / "config.json").open("r") as f:
+    with (model_save_dir / "config.json").open("r") as f:
         config = json.load(f)
 
-    with (model_save / "state_dict.pt").open("rb") as f:
+    with (model_save_dir / "state_dict.pt").open("rb") as f:
         state_dict = torch.load(f)
 
-    return ModelSave(config, state_dict)
+    config_reader = ConfigReader(config)
 
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
 
-a_sceptr_model_save = load_model_save("A_SCEPTR")
-b_sceptr_model_save = load_model_save("B_SCEPTR")
-ab_sceptr_model_save = load_model_save("AB_SCEPTR")
-ab_sceptr_large_model_save = load_model_save("AB_SCEPTR_large")
-ab_sceptr_blosum_model_save = load_model_save("AB_SCEPTR_BLOSUM")
+    name = config_reader.get_model_name()
+    tokeniser = config_reader.get_tokeniser()
+    bert = config_reader.get_bert_on_device(device)
+    bert.load_state_dict(state_dict)
+
+    return Sceptr(name=name, tokeniser=tokeniser, bert=bert, device=device)
