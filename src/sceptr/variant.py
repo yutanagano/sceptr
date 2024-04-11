6a465319769d47af052cbf06feedf7c1cdd8f1d0
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-04-11 16:10:15 +0100

    Rename model variant functions in variant submodule

--- src/sceptr/variant.py
+++ src/sceptr/variant.py
@@ -1,40 +1,40 @@
 from sceptr._model_saves import load_variant
 
 
-def ab_sceptr():
-    return load_variant("AB_SCEPTR")
+def sceptr():
+    return load_variant("SCEPTR")
 
 
-def ab_sceptr_mlm_only():
-    return load_variant("AB_SCEPTR_MLM_only")
+def sceptr_mlm_only():
+    return load_variant("SCEPTR_MLM_only")
 
 
-def ab_sceptr_classic():
-    return load_variant("AB_SCEPTR_classic")
+def sceptr_classic():
+    return load_variant("SCEPTR_classic")
 
 
-def ab_sceptr_classic_mlm_only():
-    return load_variant("AB_SCEPTR_classic_MLM_only")
+def sceptr_classic_mlm_only():
+    return load_variant("SCEPTR_classic_MLM_only")
 
 
-def ab_sceptr_cdr3_only():
-    return load_variant("AB_SCEPTR_CDR3_only")
+def sceptr_cdr3_only():
+    return load_variant("SCEPTR_CDR3_only")
 
 
-def ab_sceptr_cdr3_only_mlm_only():
-    return load_variant("AB_SCEPTR_CDR3_only_MLM_only")
+def sceptr_cdr3_only_mlm_only():
+    return load_variant("SCEPTR_CDR3_only_MLM_only")
 
 
-def ab_sceptr_large():
-    return load_variant("AB_SCEPTR_large")
+def sceptr_large():
+    return load_variant("SCEPTR_large")
 
 
-def ab_sceptr_blosum():
-    return load_variant("AB_SCEPTR_BLOSUM")
+def sceptr_blosum():
+    return load_variant("SCEPTR_BLOSUM")
 
 
-def ab_sceptr_finetuned():
-    return load_variant("AB_SCEPTR_finetuned")
+def sceptr_finetuned():
+    return load_variant("SCEPTR_finetuned")
 
 
 def a_sceptr():
