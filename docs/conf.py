a64495966018160cb986af46d8219efa76ee8bd2
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-06-09 21:18:37 +0100

    Add links to repository in sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -26,4 +26,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "repository_url": "https://github.com/yutanagano/sceptr",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_issues_button": True,
+}
 html_static_path = ["_static"]
