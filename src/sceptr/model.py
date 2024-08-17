01403e778794219c4fe7241b40eca6e3697e7afb
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-08-17 13:15:31 +0900

    Implement basic inline documentation

--- src/sceptr/model.py
+++ src/sceptr/model.py
@@ -8,14 +8,98 @@ from libtcrlm.bert import Bert
 from libtcrlm.tokeniser import Tokeniser
 from libtcrlm.tokeniser.token_indices import DefaultTokenIndex
 from libtcrlm import schema
+from turtle import Shape
 
 
 BATCH_SIZE = 512
 
 
 class ResidueRepresentations:
-    compartment_mask: ndarray
+    """
+    An object containing information necessary to interpret and operate on residue-level representations from the SCEPTR family of models.
+    Instances of this class can be obtained via the :py:func:`sceptr.calc_residue_representations` function and a method of the same name on the :py:class:`~sceptr.model.Sceptr` class.
+
+    This feature is implemented for the curious users who would like to tinker around and examine what kind of information SCEPTR focuses on at the individual amino acid residue level, and do so without completely hacking into the source code of :py:mod:`sceptr`.
+    For some examples of how to use instances of this class to make useful examinations of SCEPTR's residue-level embeddings, please refer to the "Examples" section below.
+
+    Attributes
+    ----------
+    representation_array : ndarray
+        A numpy float ndarray containing the residue-level representation data.
+        The array is of shape :math:`(N, M, D)` where :math:`N` is the number of TCRs in the original input, :math:`M` is the maximum number of residues amongst the input TCRs when put into its tokenised form, and :math:`D` is the dimensionality of the model variant that produced the result.
+
+    compartment_mask : ndarray
+        A numpy integer array containing information on which indices in the `representation_array` correspond to tokens that come from each CDR loop of the input TCRs.
+        The array is of shape :math:`(N, M)` where :math:`N` is the number of TCRs in the original input, and :math:`M` is the maximum number of residues amongst the input TCRs when put into its tokenised form.
+        Entries in `compartment_mask` have the following values:
+
+        +------------------------------+------------------+
+        | If residue at index is from: | Entry has value: |
+        +==============================+==================+
+        | None (padding token)         | 0                |
+        +------------------------------+------------------+
+        | CDR1A                        | 1                |
+        +------------------------------+------------------+
+        | CDR2A                        | 2                |
+        +------------------------------+------------------+
+        | CDR3A                        | 3                |
+        +------------------------------+------------------+
+        | CDR1B                        | 4                |
+        +------------------------------+------------------+
+        | CDR2B                        | 5                |
+        +------------------------------+------------------+
+        | CDR3B                        | 6                |
+        +------------------------------+------------------+
+
+        Within each CDR loop compartment, residues are ordered from C- to N-terminal from left to right.
+
+    Examples
+    --------
+    As an example, let's see how one could get the residue-level representations for the beta-chain CDR3 amino acid sequences of all input TCR sequences.
+    Say we have some DataFrame ``tcrs`` that contains the sequence data for four TCRs.
+
+    >>> from pandas import DataFrame
+    >>> tcrs = DataFrame(
+    ... 	data = {
+    ... 		"TRAV": ["TRAV38-1*01", "TRAV3*01", "TRAV13-2*01", "TRAV38-2/DV8*01"],
+    ... 		"CDR3A": ["CAHRSAGGGTSYGKLTF", "CAVDNARLMF", "CAERIRKGQVLTGGGNKLTF", "CAYRSAGGGTSYGKLTF"],
+    ... 		"TRBV": ["TRBV2*01", "TRBV25-1*01", "TRBV9*01", "TRBV2*01"],
+    ... 		"CDR3B": ["CASSEFQGDNEQFF", "CASSDGSFNEQFF", "CASSVGDLLTGELFF", "CASSPGTGGNEQYF"],
+    ... 	},
+    ... 	index = [0,1,2,3]
+    ... )
+    >>> print(tcrs)
+                  TRAV                 CDR3A         TRBV            CDR3B
+    0      TRAV38-1*01     CAHRSAGGGTSYGKLTF     TRBV2*01   CASSEFQGDNEQFF
+    1         TRAV3*01            CAVDNARLMF  TRBV25-1*01    CASSDGSFNEQFF
+    2      TRAV13-2*01  CAERIRKGQVLTGGGNKLTF     TRBV9*01  CASSVGDLLTGELFF
+    3  TRAV38-2/DV8*01     CAYRSAGGGTSYGKLTF     TRBV2*01   CASSPGTGGNEQYF
+
+    We can get the residue-level representations for those TCRs like so:
+
+    >>> import sceptr
+    >>> res_reps = sceptr.calc_residue_representations(tcrs)
+
+    Now, we can iterate through the residue-level representation subarray corresponding to each TCR, and filter out/obtain the representations for the beta chain CDR3 sequence.
+
+    >>> cdr3b_reps = []
+    >>> for reps, mask in zip(res_reps.representation_array, res_reps.compartment_mask):
+    ...     # reps.shape == (M, D)
+    ...     # mask.shape == (M,)
+    ...     cdr3b_rep = reps[mask == 6] # collect only the residue representations for the beta CDR3 sequence
+    ...     cdr3b_reps.append(cdr3b_rep)
+
+    Now we have a list containing four numpy ndarrays, each of which is a matrix whose row vectors are representations of individual CDR3B amino acid residues.
+
+    >>> type(cdr3b_reps[0])
+    numpy.ndarray
+    >>> cdr3b_reps[0].shape
+    (14, 64)
+
+    Note that the zeroth element of the shape tuple above is 14 because the CDR3B sequence of the first TCR in ``tcrs`` is 14 residues long, and the first element of the shape tuple is 64 because the model dimensionality of the default SCEPTR variant is 64.
+    """
     representation_array: ndarray
+    compartment_mask: ndarray
 
 
 class Sceptr:
@@ -62,6 +146,9 @@ class Sceptr:
         """
         Given multiple TCRs, map each TCR to a set of amino acid residue-level representations.
         The residue-level representations are taken from the output of the penultimate self-attention layer, and are the same ones used by the :py:func:`~sceptr.variant.average_pooling` variant when generating TCR receptor-level representations.
+
+        .. note ::
+            This method is currently only supported on SCEPTR model variants such as the default one that 1) use both the alpha and beta chains, and 2) take into account all three CDR loops from each chain.
 
         Parameters
         ----------
