b403ee706ec2e70204c6f1d2a072422b19dfcd65
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-08-21 14:09:58 +0900

    Bump version to beta 1

--- src/sceptr/__init__.py
+++ src/sceptr/__init__.py
@@ -10,7 +10,7 @@ from numpy import ndarray
 from pandas import DataFrame
 
 
-__version__ = "1.1.0-dev"
+__version__ = "1.1.0-beta.1"
 
 
 def calc_cdist_matrix(anchors: DataFrame, comparisons: DataFrame) -> ndarray:
