5ca925c36eab9082e2f2f2d17390b75a6159d20d
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-11-12 20:18:24 +0000

    feat(pyproject): start using setuptools-scm

--- src/sceptr/__init__.py
+++ src/sceptr/__init__.py
@@ -1,6 +1,7 @@
 """
-SCEPTR is a small, fast, and performant TCR representation model for alignment-free TCR analysis.
-The root module provides easy access to SCEPTR through a functional API which uses the default model.
+SCEPTR is a small, fast, and performant TCR representation model for
+alignment-free TCR analysis. The root module provides easy access to SCEPTR
+through a functional API which uses the default model.
 """
 
 from sceptr import variant
@@ -9,9 +10,6 @@ import numpy as np
 from numpy.typing import NDArray
 from pandas import DataFrame
 from typing import Optional
-
-
-__version__ = "1.1.1"
 
 
 _DEFAULT_MODEL: Optional[Sceptr] = None
@@ -27,8 +25,8 @@ def calc_cdist_matrix(
     Parameters
     ----------
     anchors : DataFrame
-        DataFrame specifying the first (anchor) collection of input TCRs.
-        It must be in the :ref:`prescribed format <data_format>`.
+        DataFrame specifying the first (anchor) collection of input TCRs. It
+        must be in the :ref:`prescribed format <data_format>`.
 
     comparisons : DataFrame
         DataFrame specifying the second (comparison) collection of input TCRs.
@@ -37,27 +35,32 @@ def calc_cdist_matrix(
     Returns
     -------
     NDArray[numpy.float32]
-        A 2D numpy ndarray representing a cdist matrix between TCRs from `anchors` and `comparisons`.
-        The returned array will have shape :math:`(X, Y)` where :math:`X` is the number of TCRs in `anchors` and :math:`Y` is the number of TCRs in `comparisons`.
+        A 2D numpy ndarray representing a cdist matrix between TCRs from
+        `anchors` and `comparisons`. The returned array will have shape
+        :math:`(X, Y)` where :math:`X` is the number of TCRs in `anchors` and
+        :math:`Y` is the number of TCRs in `comparisons`.
     """
     return _get_default_model().calc_cdist_matrix(anchors, comparisons)
 
 
 def calc_pdist_vector(instances: DataFrame) -> NDArray[np.float32]:
     r"""
-    Generate a pdist vector of distances between each pair of TCRs in the input data.
+    Generate a pdist vector of distances between each pair of TCRs in the input
+    data.
 
     Parameters
     ----------
     instances : DataFrame
-        DataFrame specifying the input TCRs.
-        It must be in the :ref:`prescribed format <data_format>`.
+        DataFrame specifying the input TCRs. It must be in the :ref:`prescribed
+        format <data_format>`.
 
     Returns
     -------
     NDArray[numpy.float32]
-        A 1D numpy ndarray representing a pdist vector of distances between each pair of TCRs in `instances`.
-        The returned array will have shape :math:`(\frac{1}{2}N(N-1),)`, where :math:`N` is the number of TCRs in `instances`.
+        A 1D numpy ndarray representing a pdist vector of distances between
+        each pair of TCRs in `instances`. The returned array will have shape
+        :math:`(\frac{1}{2}N(N-1),)`, where :math:`N` is the number of TCRs in
+        `instances`.
     """
     return _get_default_model().calc_pdist_vector(instances)
 
@@ -69,49 +72,60 @@ def calc_vector_representations(instances: DataFrame) -> NDArray[np.float32]:
     Parameters
     ----------
     instances : DataFrame
-        DataFrame specifying the input TCRs.
-        It must be in the :ref:`prescribed format <data_format>`.
+        DataFrame specifying the input TCRs. It must be in the :ref:`prescribed
+        format <data_format>`.
 
     Returns
     -------
     NDArray[numpy.float32]
-        A 2D numpy ndarray object where every row vector corresponds to a row in `instances`.
-        The returned array will have shape :math:`(N, 64)` where :math:`N` is the number of TCRs in `instances`.
+        A 2D numpy ndarray object where every row vector corresponds to a row
+        in `instances`. The returned array will have shape :math:`(N, 64)`
+        where :math:`N` is the number of TCRs in `instances`.
     """
     return _get_default_model().calc_vector_representations(instances)
 
 
 def calc_residue_representations(instances: DataFrame) -> ResidueRepresentations:
     """
-    Map each TCR to a set of amino acid residue-level representations.
-    The residue-level representations are the output of the penultimate self-attention layer, as also used by the :py:func:`~sceptr.variant.average_pooling` variant when generating TCR receptor-level representations.
+    Map each TCR to a set of amino acid residue-level representations. The
+    residue-level representations are the output of the penultimate
+    self-attention layer, as also used by the
+    :py:func:`~sceptr.variant.average_pooling` variant when generating TCR
+    receptor-level representations.
 
     Parameters
     ----------
     instances : DataFrame
-        DataFrame specifying the input TCRs.
-        It must be in the :ref:`prescribed format <data_format>`.
+        DataFrame specifying the input TCRs. It must be in the :ref:`prescribed
+        format <data_format>`.
 
     Returns
     -------
     :py:class:`~sceptr.model.ResidueRepresentations`
-        An array of representation vectors for each amino acid residue in the tokenised forms of the input TCRs.
-        For details on how to interpret/use this output, please refer to the documentation for :py:class:`~sceptr.model.ResidueRepresentations`.
+        An array of representation vectors for each amino acid residue in the
+        tokenised forms of the input TCRs. For details on how to interpret/use
+        this output, please refer to the documentation for
+        :py:class:`~sceptr.model.ResidueRepresentations`.
     """
     return _get_default_model().calc_residue_representations(instances)
 
 
 def enable_hardware_acceleration() -> None:
     """
-    Instruct SCEPTR to detect and use available hardware acceleration, such as CUDA.
+    Instruct SCEPTR to detect and use available hardware acceleration, such as
+    CUDA.
 
-    While hardware acceleration is toggled on by default, it can be turned off manually by calling :py:func:`sceptr.disable_hardware_acceleration`.
-    This function allows you to turn the setting back on.
+    While hardware acceleration is toggled on by default, it can be turned off
+    manually by calling :py:func:`sceptr.disable_hardware_acceleration`. This
+    function allows you to turn the setting back on.
 
     .. note ::
-        Toggling this setting will affect the behaviour of the :ref:`functional API <functional_api>` and any new :ref:`variants <model_variants>` instantiated after the fucntion call.
-        However, any variants instantiated before the call will remain unaffected.
-        To enable hardware acceleration for existing model instances, use :py:meth:`sceptr.model.Sceptr.enable_hardware_acceleration`.
+        Toggling this setting will affect the behaviour of the :ref:`functional
+        API <functional_api>` and any new :ref:`variants <model_variants>`
+        instantiated after the fucntion call. However, any variants
+        instantiated before the call will remain unaffected. To enable hardware
+        acceleration for existing model instances, use
+        :py:meth:`sceptr.model.Sceptr.enable_hardware_acceleration`.
     """
     global _USE_HARDWARE_ACCELERATION
     _USE_HARDWARE_ACCELERATION = True
@@ -122,17 +136,23 @@ def enable_hardware_acceleration() -> None:
 
 def disable_hardware_acceleration() -> None:
     """
-    Instruct SCEPTR to ignore hardware acceleration options and only use the CPU.
+    Instruct SCEPTR to ignore hardware acceleration options and only use the
+    CPU.
 
-    By default, SCEPTR will look for available hardware acceleration devices such as CUDA-enabled GPUs and perform computations there.
-    However, in some cases it may be favourable to explicitly keep models on the CPU (e.g. a CUDA-enabled GPU is available but does not have sufficient VRAM for your use case).
-    This function is useful for such scenarios.
-    This setting can be reversed using :py:func:`sceptr.enable_hardware_acceleration`.
+    By default, SCEPTR will look for available hardware acceleration devices
+    such as CUDA-enabled GPUs and perform computations there. However, in some
+    cases it may be favourable to explicitly keep models on the CPU (e.g. a
+    CUDA-enabled GPU is available but does not have sufficient VRAM for your
+    use case). This function is useful for such scenarios. This setting can be
+    reversed using :py:func:`sceptr.enable_hardware_acceleration`.
 
     .. note ::
-        Toggling this setting will affect the behaviour of the :ref:`functional API <functional_api>` and any new :ref:`variants <model_variants>` instantiated after the fucntion call.
-        However, any variants instantiated before the call will remain unaffected.
-        To disable hardware acceleration for existing model instances, use :py:meth:`sceptr.model.Sceptr.disable_hardware_acceleration`.
+        Toggling this setting will affect the behaviour of the :ref:`functional
+        API <functional_api>` and any new :ref:`variants <model_variants>`
+        instantiated after the fucntion call. However, any variants
+        instantiated before the call will remain unaffected. To disable
+        hardware acceleration for existing model instances, use
+        :py:meth:`sceptr.model.Sceptr.disable_hardware_acceleration`.
     """
     global _USE_HARDWARE_ACCELERATION
     _USE_HARDWARE_ACCELERATION = False
