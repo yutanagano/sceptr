d0d4255c0a0855e66088eeccbeb192285e342f31
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-11-12 22:19:21 +0000

    feat(tests): use fixture for proper setup and teardown of musmusculus mode

--- tests/test_musmusculus_support.py
+++ tests/test_musmusculus_support.py
@@ -1,4 +1,5 @@
 import sceptr
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -7,10 +8,14 @@ sceptr.disable_hardware_acceleration()
 
 
 @pytest.fixture
-def dummy_data_homosapiens():
-    df = pd.read_csv("tests/mock_data.csv")
-    return df
+def dummy_data():
+    sceptr.setup("musmusculus")
+    yield pd.read_csv("tests/mock_data_musmusculus.csv")
+    sceptr.setup("homosapiens")
 
 
-def test_musmusculus_support(dummy_data_homosapiens):
-    _ = sceptr.calc_vector_representations(dummy_data_homosapiens)
+def test_musmusculus_support(dummy_data):
+    result = sceptr.calc_vector_representations(dummy_data)
+
+    assert isinstance(result, np.ndarray)
+    assert result.shape == (3, 64)
